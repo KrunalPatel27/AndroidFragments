59c4c93d0618e109edb75f4a38bef9daada55b79
Krunal Patel <kpate236@uic.edu>
2018-03-22 02:10:32 -0500

    update

--- project3_A1/app/src/main/java/com/example/krunal/project3_a1/MainActivity.java
+++ project3_A1/app/src/main/java/com/example/krunal/project3_a1/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.krunal.project3_a1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,6 +10,7 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
     Button button1;
     Button button2;
+    Boolean attractions;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,8 +21,27 @@ public class MainActivity extends AppCompatActivity {
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                Toast.makeText(getApplicationContext(), "Lookup Attractions", Toast.LENGTH_SHORT).show();
+                attractions = true;
+                startAnotherApp();
             }
         });
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Lookup Restaurants", Toast.LENGTH_SHORT).show();
+                attractions = false;
+                startAnotherApp();
+            }
+        });
+    }
+    protected void startAnotherApp(){
+        Intent intent = getPackageManager().getLaunchIntentForPackage("com.example.project3_a2");
+        if(intent == null) return;
+        if(attractions){
+            intent.putExtra("type", true);
+        } else
+            intent.putExtra("type", false);
+        startActivity(intent);
     }
 }
